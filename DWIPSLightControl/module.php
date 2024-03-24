46bdf88e0463462c2c8299389bb3db3df128fb7f
Daniel81W <it@dan-web.de>
2024-03-24 09:22:35 +0100

    additional properties

---
+++ DWIPSLightControl/module.php
@@ -0,0 +1,50 @@
+<?php /** @noinspection PhpRedundantClosingTagInspection */
+
+//include_once("/var/lib/symcon/modules/DWIPSLib/libs/astro.php");
+	class DWIPSLightControl extends IPSModule {
+
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+
+            $this->RegisterPropertyString("Lights", "");
+            $this->RegisterVariableInteger("oncount", $this->Translate("Number of lights switched on"),"",1);
+
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+		/**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wie folgt zur Verfügung gestellt:
+        *
+        * DWIPSShutter_UpdateSunrise($id);
+        *
+        */
+
+		public function ReceiveData($JSONString) {
+
+		}
+
+		public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
+
+		}
+
+        public function RegisterLight($lightId){
+
+        }
+
+    }
+?>
