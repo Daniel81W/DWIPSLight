138b37fb6ac64058e46a82165f04b0a0d3cb2517
Daniel81W <it@dan-web.de>
2024-04-02 18:30:50 +0200

    Initial push

--- DWIPSKNXLight/module.php
+++ DWIPSKNXLight/module.php
@@ -57,8 +57,6 @@
                 $this->UnregisterVariable("brightness");
             }
 
-
-
             if(count(IPS_GetInstanceListByModuleID("{A3BDFBC5-CDDB-5656-F265-DB4132FEE4B0}")) > 0) {
                 /** @noinspection PhpUndefinedFunctionInspection */
                 DWIPSLightControl_RegisterLight(IPS_GetInstanceListByModuleID("{A3BDFBC5-CDDB-5656-F265-DB4132FEE4B0}")[0], $this->InstanceID);
