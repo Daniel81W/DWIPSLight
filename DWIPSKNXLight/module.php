9d14ce42c9ed1c5355c368b18e97aa43a8049d23
Daniel81W <it@dan-web.de>
2024-04-02 19:02:22 +0200

    Code nach und nach hinzufügen, um Fehlerquelle zu finden

--- DWIPSKNXLight/module.php
+++ DWIPSKNXLight/module.php
@@ -61,6 +61,7 @@
                 /** @noinspection PhpUndefinedFunctionInspection */
                 $this->SendDebug("",count(IPS_GetInstanceListByModuleID("{A3BDFBC5-CDDB-5656-F265-DB4132FEE4B0}")),0);
                 $this->SendDebug("",$this->InstanceID,0);
+                $lightControl = IPS_GetInstanceListByModuleID("{A3BDFBC5-CDDB-5656-F265-DB4132FEE4B0}");
                 //DWIPSLightControl_RegisterLight(IPS_GetInstanceListByModuleID("{A3BDFBC5-CDDB-5656-F265-DB4132FEE4B0}")[0], $this->InstanceID);
             }
 		}
