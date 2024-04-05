3651b4875e2373815a7e3bd01d361284bc81fa87
Daniel81W <it@dan-web.de>
2024-04-05 17:18:55 +0200

    Mit LightGroup begonnen

--- DWIPSLightGroup/module.php
+++ DWIPSLightGroup/module.php
@@ -27,6 +27,7 @@
 
             //TODO Array mit LichtIDs laden
             $lightIDArray = json_decode($this->ReadPropertyString("Lights"));
+            $this->SendDebug("", print_r($lightIDArray),0);
             foreach ($lightIDArray as $l){
                 $this->SendDebug("", print_r($l),0);
             }
